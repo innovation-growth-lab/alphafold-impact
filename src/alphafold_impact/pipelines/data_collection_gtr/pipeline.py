13dc9706140b7100b1b3d81f5f0935c638a5a2a5
ampudia19 <dampudiavicente@gmail.com>
2023-12-15 19:55:05 +0100

    split pipelines for sequential runs

--- src/alphafold_impact/pipelines/data_collection_gtr/pipeline.py
+++ src/alphafold_impact/pipelines/data_collection_gtr/pipeline.py
@@ -8,7 +8,7 @@ use the following command:
 """
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import fetch_gtr_data, preprocess_data_to_df, preprocess_organisations  # pylint: disable=E0401
+from .nodes import fetch_gtr_data, preprocess_data_to_df  # pylint: disable=E0401
 
 
 def create_pipeline(**kwargs) -> Pipeline:  # pylint: disable=W0613
@@ -16,26 +16,46 @@ def create_pipeline(**kwargs) -> Pipeline:  # pylint: disable=W0613
 
     Returns:
         Pipeline: The data collection pipeline.
-    """    
-    return pipeline(
+    """
+    organisations_pipeline = pipeline(
         [
             node(
                 func=fetch_gtr_data,
-                inputs="params:param_requests",
+                inputs=["params:param_requests", "params:orgs"],
                 outputs="gtr_raw_organisations",
-                name="fetch_gtr_data_node",
+                tags=["orgs"],
             ),
             node(
                 func=preprocess_data_to_df,
-                inputs="gtr_raw_organisations",
-                outputs="gtr_df_organisations",
-                name="preprocess_data_to_df_node",
-            ),
-            node(
-                func=preprocess_organisations,
-                inputs="gtr_df_organisations",
+                inputs=["gtr_raw_organisations", "params:orgs"],
                 outputs="gtr_organisations",
-                name="preprocess_organisations_node",
+                tags=["orgs"],
             ),
         ]
     )
+    funds_pipeline = pipeline(
+        [
+            node(
+                func=fetch_gtr_data,
+                inputs=["params:param_requests", "params:funds"],
+                outputs="gtr_raw_funds",
+                tags=["funds"],
+            ),
+            node(
+                func=preprocess_data_to_df,
+                inputs=["gtr_raw_funds", "params:funds"],
+                outputs="gtr_funds",
+                tags=["funds"],
+            ),
+        ]
+    )
+
+    return organisations_pipeline + funds_pipeline
+
+
+# [TODO] I would like to have a pipeline that can iterate over a number of possible endpoints for GtR data,
+#       but I'm not sure how to do this with Kedro. I think I need to define a pipeline for each endpoint,
+#       and then have a pipeline that calls each of these pipelines in turn. For now the best I've managed
+#       is to have two separate pipelines, one for organisations and one for funds. I can use tags to
+#       distinguish between the two. There's probably a better solution using modular pipelines:
+#       https://docs.kedro.org/en/stable/nodes_and_pipelines/modular_pipelines.html
