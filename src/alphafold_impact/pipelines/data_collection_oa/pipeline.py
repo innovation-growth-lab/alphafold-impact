364dc598dcdc3da0c001965a8a6223f7d464fc67
Jack Rasala <jack@jrds.io>
2023-12-22 12:42:48 +0000

    Openalex works from concept ids

--- src/alphafold_impact/pipelines/data_collection_oa/pipeline.py
+++ src/alphafold_impact/pipelines/data_collection_oa/pipeline.py
@@ -1,5 +1,7 @@
 """
-Pipeline for collecting incoming (cites) or outgoing (references) citations for a given work.
+Pipeline for collecting data using the OpenAlex API.
+
+There is a pipeline for collecting incoming (cites) or outgoing (references) citations for a given work.
 
 The main pipeline is a template pipeline that is used to create two pipelines, using the 
 `modular_pipeline` function. The two pipelines are `incoming_pipeline` and
@@ -8,7 +10,7 @@ The main pipeline is a template pipeline that is used to create two pipelines, u
 Note that the output folders are also dynamically named based on the direction of the
 citations.
 
-To run this pipeline, use the following command:
+To run the full pipeline, use the following command:
     
         $ kedro run --pipeline data_collection_oa
 
@@ -33,12 +35,23 @@ To run this pipeline, use the following command:
 The pipeline will save the data to the following location:
             s3://alphafold-impact/data/02_intermediate/openalex/works/cited_by/downstream/<work_id>
 using a partitioned dataset.
+
+The pipeline also includes `works_for_concepts_and_years` that is used to collect
+OpenAlex works for a specified list of OpenAlex concepts and publication years.
+
+To run this pipeline, use the following command:
+
+            $ kedro run --pipeline data_collection_oa --tags works_for_concepts_and_years
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
 from kedro.pipeline.modular_pipeline import pipeline as mpl
 
-from .nodes import collect_citation_papers, load_work_ids
+from .nodes import (
+    collect_citation_papers,
+    load_work_ids,
+    retrieve_oa_works_for_concepts_and_years,
+)
 
 template_pipeline = pipeline(
     [
@@ -58,14 +71,14 @@ template_pipeline = pipeline(
 incoming_pipeline = mpl(
     pipe=template_pipeline,
     parameters={"params:direction.template": "params:direction.incoming"},
-    outputs={"template": "works_outgoing_citations"},
+    outputs={"template": "oa_int_works_outgoing_citations"},
     tags="incoming",
 )
 
 outgoing_pipeline = mpl(
     pipe=template_pipeline,
     parameters={"params:direction.template": "params:direction.outgoing"},
-    outputs={"template": "works_incoming_citations"},
+    outputs={"template": "oa_int_works_incoming_citations"},
     tags="outgoing",
 )
 
@@ -75,7 +88,7 @@ downstream_impact_pipeline = pipeline(
             func=load_work_ids,
             inputs={
                 "work_id": "params:get.work_id",
-                "dataset": "works_incoming_citations",
+                "dataset": "oa_int_works_incoming_citations",
             },
             outputs="work_ids",
         ),
@@ -87,12 +100,31 @@ downstream_impact_pipeline = pipeline(
                 "work_ids": "work_ids",
                 "direction": "params:direction.incoming",
             },
-            outputs="downstream_incoming_citations",
+            outputs="oa_int_downstream_incoming_citations",
         ),
     ],
     tags="downstream_impact",
 )
 
+works_for_concepts_and_years = pipeline(
+    [
+        node(
+            func=retrieve_oa_works_for_concepts_and_years,
+            inputs=[
+                "params:test_concept_ids",
+                "params:test_publication_years",
+            ],
+            outputs="oa_raw_works_for_concepts_and_years",
+        ),
+    ],
+    tags="works_for_concepts_and_years",
+)
+
 
 def create_pipeline(**kwargs) -> Pipeline:  # pylint: disable=C0116,W0613
-    return incoming_pipeline + outgoing_pipeline + downstream_impact_pipeline
+    return (
+        incoming_pipeline
+        + outgoing_pipeline
+        + downstream_impact_pipeline
+        + works_for_concepts_and_years
+    )
