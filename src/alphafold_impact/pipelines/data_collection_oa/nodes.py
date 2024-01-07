a463b44fa5b37b34b820c2ea10594140607f3ba3
Jack Rasala <jack@jrds.io>
2024-01-07 21:59:12 +0000

    Remove duplicated function

--- src/alphafold_impact/pipelines/data_collection_oa/nodes.py
+++ src/alphafold_impact/pipelines/data_collection_oa/nodes.py
@@ -25,64 +25,6 @@ def retrieve_oa_works_for_single_concept_and_year(
     base_url = "https://api.openalex.org/works"
     works = []
     next_cursor = "*"
-    first_request = True
-
-    while True:
-        params = {
-            "filter": f"concepts.id:{concept_id},publication_year:{year}",
-            "per_page": per_page,
-            "cursor": next_cursor,
-        }
-        response = requests.get(base_url, params=params)
-        if response.status_code == 200:
-            data = response.json()
-            current_works = data.get("results", [])
-            if first_request:
-                # Calculate the total number of pages from the first response
-                total_count = data.get("meta", {}).get("count", 0)
-                total_pages = math.ceil(total_count / per_page)
-                current_page = 1
-                first_request = False
-
-            if not current_works:
-                break
-
-            logger.info(
-                f"Processing concept {concept_id.upper()} page {current_page}/{total_pages}"
-            )
-            works.extend(current_works)
-            current_page += 1
-
-            # Retrieve the next cursor from the response
-            next_cursor = data.get("meta", {}).get("next_cursor")
-            if not next_cursor:
-                break
-        else:
-            logger.error(
-                f"Error fetching data for concept {concept_id} and year {year}: {response.status_code}"
-            )
-            break
-
-    return works
-
-
-def retrieve_oa_works_for_single_concept_and_year(
-    concept_id: str, year: int, per_page: int
-) -> List[dict]:
-    """
-    Retrieves OpenAlex works for a single concept ID and publication year.
-
-    Args:
-        concept_id (str): The OpenAlex concept ID.
-        year (int): The publication year.
-        per_page (int): The number of results to retrieve per page.
-
-    Returns:
-        List[dict]: A list of works for the given concept ID and year.
-    """
-    base_url = "https://api.openalex.org/works"
-    works = []
-    next_cursor = "*"
     current_page = 1
 
     while True:
@@ -101,10 +43,10 @@ def retrieve_oa_works_for_single_concept_and_year(
                 total_pages = math.ceil(total_count / per_page)
 
             if not current_works:
-                break  # Break if there are no more works
+                break
 
             logger.info(
-                f"Processing page {current_page}/{total_pages} for concept {concept_id.upper()}"
+                f"Processing concept {concept_id.upper()} page {current_page}/{total_pages}"
             )
             works.extend(current_works)
             current_page += 1
@@ -112,7 +54,7 @@ def retrieve_oa_works_for_single_concept_and_year(
             # Retrieve the next cursor from the response
             next_cursor = data.get("meta", {}).get("next_cursor")
             if not next_cursor:
-                break  # Break if there's no next cursor
+                break
         else:
             logger.error(
                 f"Error fetching data for concept {concept_id} and year {year}: {response.status_code}"
