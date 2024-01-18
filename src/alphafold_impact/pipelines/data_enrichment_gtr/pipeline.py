7d1056c34edbb3f470898a1282ee78d8ce6893de
ampudia19 <dampudiavicente@gmail.com>
2024-01-18 13:34:11 +0100

    update gtr code

--- src/alphafold_impact/pipelines/data_enrichment_gtr/pipeline.py
+++ src/alphafold_impact/pipelines/data_enrichment_gtr/pipeline.py
@@ -13,49 +13,68 @@ from alphafold_impact.pipelines.data_collection_oa.nodes import collect_papers
 from .nodes import (
     preprocess_publication_doi,
     create_list_doi_inputs,
-    create_dictionary_doi_to_oa,
+    test_node,
+    load_referenced_work_ids,
 )
 
+LABEL = "gtr"
 
 def create_pipeline(**kwargs) -> Pipeline:  # pylint: disable=C0116,W0613
     return pipeline(
         [
             node(
                 func=preprocess_publication_doi,
-                inputs="gtr_intermediate_publications",
-                outputs="gtr_preprocessed_publications",
+                inputs="gtr.collection.outcomes/publications.intermediate",
+                outputs=f"{LABEL}.enrichment.publications.preproc",
+                tags="gtr.publications",
             ),
             node(
                 func=create_list_doi_inputs,
-                inputs="gtr_preprocessed_publications",
-                outputs="gtr_doi_list",
+                inputs=f"{LABEL}.enrichment.publications.preproc",
+                outputs=f"{LABEL}.enrichment.publications.doi.list",
+                tags="gtr.publications",
             ),
             node(
                 func=collect_papers,
                 inputs={
-                    "mailto": "params:api.mailto",
-                    "perpage": "params:api.perpage",
-                    "work_ids": "gtr_doi_list",
-                    "filter_by": "params:oa_gtr.filter",
+                    "mailto": "params:oa.api.mailto",
+                    "perpage": "params:oa.api.perpage",
+                    "work_ids": f"{LABEL}.enrichment.publications.doi.list",
+                    "filter_by": "params:oa.gtr.filter_doi",
                     "group_work_ids": "params:oa.group_work_ids",
-                    "eager_loading": "params:oa.eager_loading",
+                    "timestamp_keys": "params:oa.timestamp_keys",
+                    "parallelise": "params:oa.parallelise",
                 },
-                outputs="gtr_dict_matches",
+                outputs=f"{LABEL}.enrichment.publications.oa.tmp",
+                tags="gtr.publications",
             ),
             node(
-                func=create_dictionary_doi_to_oa,
-                inputs="gtr_dict_matches",
-                outputs="gtr_intermediate_publications_oa_dict",
+                func=load_referenced_work_ids,
+                inputs=f"{LABEL}.enrichment.publications.oa.tmp",
+                outputs=[
+                    f"{LABEL}.enrichment.publications.oa.list",
+                    f"{LABEL}.enrichment.publications.oa.dict",
+                ],
+                tags="gtr.citations",
             ),
+            # node(
+            #     func=create_dictionary_doi_to_oa,
+            #     inputs="gtr.publications.oa.list",
+            #     outputs="gtr.publications.dict",
+            # ),
             node(
                 func=collect_papers,
                 inputs={
-                    "mailto": "params:api.mailto",
-                    "perpage": "params:api.perpage",
-                    "work_ids": "gtr_intermediate_publications_oa_dict",
-                    "filter_by": "params:direction.cited_by", # 
+                    "mailto": "params:oa.api.mailto",
+                    "perpage": "params:oa.api.perpage",
+                    "work_ids": f"{LABEL}.enrichment.publications.oa.list",
+                    "filter_by": "params:oa.gtr.filter_oa",
+                    "group_work_ids": "params:oa.group_work_ids",
+                    "timestamp_keys": "params:oa.timestamp_keys",
+                    "parallelise": "params:oa.parallelise",
                 },
-                outputs="gtr_intermediate_publication_citations",
+                outputs=f"{LABEL}.enrichment.publications.oa.citations",
+                tags="gtr.citations",
             ),
-        ]
+        ],
     )
