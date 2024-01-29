4dc3a8932b42494fbb2ef1fbe5e34f3b19369498
Jack Rasala <jack@jrds.io>
2024-01-29 14:06:52 +0000

    Update pipeline

--- src/alphafold_impact/pipelines/data_collection_nih_clinical_trials/pipeline.py
+++ src/alphafold_impact/pipelines/data_collection_nih_clinical_trials/pipeline.py
@@ -15,11 +15,11 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=collect_and_save_nih_clinical_trials,
                 inputs={
-                    "nih_ct_download_url": "params:nih_ct_download_url",
-                    "nih_ct_zip_save_path": "params:nih_ct_zip_save_path",
-                    "nih_ct_extract_path": "params:nih_ct_extract_path",
+                    "nih_ct_download_url": "params:nih.data_collection.clinical_trials.download_url",
+                    "nih_ct_zip_save_path": "params:nih.data_collection.clinical_trials.zip_save_path",
+                    "nih_ct_extract_path": "params:nih.data_collection.clinical_trials.extract_path",
                 },
-                outputs="nih_raw_clinical_trials",
+                outputs="nih.data_collection.clinical_trials.raw",
             ),
         ]
     )
