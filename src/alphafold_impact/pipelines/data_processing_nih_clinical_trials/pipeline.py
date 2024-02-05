03a9a61c51165287d0c72cdb94e3e3239a198f86
Jack Rasala <jack@jrds.io>
2024-02-05 14:41:46 +0000

    Update processing to data_processing

--- src/alphafold_impact/pipelines/data_processing_nih_clinical_trials/pipeline.py
+++ src/alphafold_impact/pipelines/data_processing_nih_clinical_trials/pipeline.py
@@ -15,13 +15,13 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=trials_with_references,
                 inputs="nih.data_collection.clinical_trials.raw",
-                outputs="nih.processing.clinical_trials_with_references.intermediate",
+                outputs="nih.data_processing.clinical_trials_with_references.intermediate",
                 tags="trials_with_references",
             ),
             node(
                 func=trials_links_to_papers,
-                inputs="nih.processing.clinical_trials_with_references.intermediate",
-                outputs="nih.processing.clinical_trials_links_to_papers.intermediate",
+                inputs="nih.data_processing.clinical_trials_with_references.intermediate",
+                outputs="nih.data_processing.clinical_trials_links_to_papers.intermediate",
                 tags="trials_links_to_papers",
             ),
         ],
