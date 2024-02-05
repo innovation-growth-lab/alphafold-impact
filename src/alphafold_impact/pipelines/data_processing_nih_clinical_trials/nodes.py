4295b15b049f17836e48ee6ea40d08a297a21b98
Jack Rasala <jack@jrds.io>
2024-02-05 14:49:02 +0000

    Move inner functions to the top of the file

--- src/alphafold_impact/pipelines/data_processing_nih_clinical_trials/nodes.py
+++ src/alphafold_impact/pipelines/data_processing_nih_clinical_trials/nodes.py
@@ -21,43 +21,6 @@ import pandas as pd
 logger = logging.getLogger(__name__)
 
 
-def trials_with_references(
-    nih_clinical_trials_partitions: Dict[str, Callable[[], Any]]
-) -> pd.DataFrame:
-    """Loads NIH clinical trials from partitioned dataset, filters out trials
-    that do not reference a paper, concatenates results into one DataFrame.
-
-    Args:
-        nih_clinical_trials_partitions (dict): Partitioned dataset of NIH clinical trials.
-
-    Returns:
-        DataFrame of NIH clinical trials with a paper reference.
-    """
-    ct_with_refs_combined = pd.DataFrame()
-    n_partitions = len(nih_clinical_trials_partitions)
-
-    for idx, (_, ct_partition_load_func) in enumerate(
-        sorted(nih_clinical_trials_partitions.items()), start=1
-    ):
-        ct_partition_data_with_refs = ct_partition_load_func().query(
-            "`ProtocolSection.ReferencesModule.ReferenceList.Reference`.notna()"
-        )
-
-        ct_with_refs_combined = pd.concat(
-            [ct_with_refs_combined, ct_partition_data_with_refs],
-            ignore_index=True,
-            join="outer",
-        )
-
-        logger.info(
-            "Concatenated clinical trials with references from partition %s/%s",
-            idx,
-            n_partitions,
-        )
-
-    return ct_with_refs_combined
-
-
 def _reference_string_to_df(row: pd.Series) -> pd.DataFrame:
     """
     Processes a row of the DataFrame, converting the reference string to a list of dictionaries.
@@ -95,6 +58,43 @@ def _extract_doi(citation: str) -> str:
     if doi.endswith("."):
         doi = doi[:-1]
     return doi
+
+
+def trials_with_references(
+    nih_clinical_trials_partitions: Dict[str, Callable[[], Any]]
+) -> pd.DataFrame:
+    """Loads NIH clinical trials from partitioned dataset, filters out trials
+    that do not reference a paper, concatenates results into one DataFrame.
+
+    Args:
+        nih_clinical_trials_partitions (dict): Partitioned dataset of NIH clinical trials.
+
+    Returns:
+        DataFrame of NIH clinical trials with a paper reference.
+    """
+    ct_with_refs_combined = pd.DataFrame()
+    n_partitions = len(nih_clinical_trials_partitions)
+
+    for idx, (_, ct_partition_load_func) in enumerate(
+        sorted(nih_clinical_trials_partitions.items()), start=1
+    ):
+        ct_partition_data_with_refs = ct_partition_load_func().query(
+            "`ProtocolSection.ReferencesModule.ReferenceList.Reference`.notna()"
+        )
+
+        ct_with_refs_combined = pd.concat(
+            [ct_with_refs_combined, ct_partition_data_with_refs],
+            ignore_index=True,
+            join="outer",
+        )
+
+        logger.info(
+            "Concatenated clinical trials with references from partition %s/%s",
+            idx,
+            n_partitions,
+        )
+
+    return ct_with_refs_combined
 
 
 def trials_links_to_papers(
