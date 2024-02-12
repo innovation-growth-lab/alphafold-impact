b8af3e083716dd58feb8186637f894cb49881d02
ampudia19 <dampudiavicente@gmail.com>
2024-02-12 19:36:33 +0100

    implemented levels

--- src/alphafold_impact/pipelines/data_collection_s2/nodes.py
+++ src/alphafold_impact/pipelines/data_collection_s2/nodes.py
@@ -8,7 +8,7 @@ Functions:
 """
 
 import logging
-from typing import Sequence, Dict, Tuple
+from typing import Sequence, Dict, Tuple, Union
 import re
 import pandas as pd
 import requests
@@ -100,7 +100,7 @@ def fetch_citation_details(
 
 def get_alphafold_citation_details(
     work_ids: Sequence[Tuple[str, str, str, str]], af_doi: str, **kwargs
-) -> Dict[str, Dict[str, str]]:
+) -> pd.DataFrame:
     """Retrieves citation details for a given list of work IDs and filters
     them based on the specified AlphaFold DOI.
 
@@ -111,9 +111,14 @@ def get_alphafold_citation_details(
             fetch_citation_details function.
 
     Returns:
-        Dict[str, Dict[str, str]]: A dictionary containing the filtered
-            citation details, where the keys are the work IDs and the values
-            are the corresponding citation details.
+        pd.DataFrame: A DataFrame containing the citation details for the
+        specified work IDs, filtered by the AlphaFold DOI. The DataFrame
+        has the following columns:
+        - oa: The work ID.
+        - citedPaper: The details of the cited paper, including the title,
+          authors, and publication information.
+        - otherColumns: Any additional columns present in the citation details.
+
     """
     citation_details = {}
     for work_id_tuple in work_ids:
@@ -130,7 +135,12 @@ def get_alphafold_citation_details(
                     if (eids := item["citedPaper"]["externalIds"]) is not None:
                         if eids.get("DOI", "") == af_doi:
                             logger.info("Found citation details using %s", work_id)
-                            citation_details[oa] = item
+                            citation_details[oa] = {
+                                "item": item,
+                                "doi": doi,
+                                "mag": mag,
+                                "pmid": pmid,
+                            }
                             break
                 else:
                     logger.warning("No relevant citation found in %s", work_id)
@@ -140,16 +150,189 @@ def get_alphafold_citation_details(
                 logger.warning("Failed to fetch citation details for %s", work_id)
 
     # explode multiple citations
-    rows = []
-    for child, details in citation_details.items():
-        intents = details.get("intents", [])
-        contexts = details.get("contexts", [])
-        for intent, context in zip(intents, contexts):
-            rows.append(["W3177828909", child, intent, context])
+    citation_details = process_citations(citations=citation_details, parent=af_doi)
+    return {af_doi: citation_details}, [tuple([af_doi, 34265844])]
 
-    # create dataframe
-    citation_details = pd.DataFrame(
-        rows, columns=["parent", "child", "intent", "context"]
+
+#  list(set(citation_details["child_doi"].to_list()))
+
+
+def fetch_citation_strength(
+    parent_data: Sequence[Dict[str, AbstractDataset]],
+    logs: Sequence[Tuple[str, str]],
+    **kwargs,
+) -> pd.DataFrame:
+    """
+    Fetches the citation strength for a new level based on the parent data and logs.
+
+    Args:
+        parent_data (Sequence[Dict[str, AbstractDataset]]): The parent data containing 
+            the datasets.
+        logs (Sequence[Tuple[str, str]]): The logs containing the paper IDs.
+        **kwargs: Additional keyword arguments.
+
+    Returns:
+        pd.DataFrame: The citation strength data.
+
+    Yields:
+        Dict[str, List[Dict[str, Any]]], List[List[str]]: The citation details for each 
+            paper and the processed paper IDs.
+    """
+    logger.info("Fetching citation strength for a new level")
+    processed_paper_ids = set(tuple(log) for log in logs)
+    paper_ids_to_process = set()
+
+    for _, loader in parent_data.items():
+        data = loader()
+        set_of_papers = (
+            set(zip(data["child_doi"].to_list(), data["child_pmid"].to_list()))
+            - processed_paper_ids
+        )
+        paper_ids_to_process.update(set_of_papers)
+
+    while paper_ids_to_process:
+        current_paper = paper_ids_to_process.pop()
+        logger.info(
+            "Fetching citation details for %s. Papers remaining: %s",
+            current_paper,
+            len(paper_ids_to_process),
+        )
+        try:
+            citation_details = fetch_citation_details(
+                work_id=f"DOI:{current_paper[0]}", **kwargs
+            )
+        except requests.exceptions.HTTPError:
+            pass
+
+        if not citation_details and current_paper[1]:
+            try:
+                citation_details = fetch_citation_details(
+                    work_id=f"PMID:{current_paper[1]}", **kwargs
+                )
+            except requests.exceptions.HTTPError:
+                continue
+
+        logger.info("Fetched citation details for %s", current_paper)
+        citation_details = process_citations(
+            citations=citation_details, parent=current_paper
+        )
+
+        processed_paper_ids.add(current_paper)
+
+        logger.info("Fetched citation details for %s", current_paper)
+        yield {current_paper[0]: citation_details}, [current_paper]
+
+    logger.info(
+        "Finished fetching citation strength for a new level. Flushing IDs list."
     )
 
-    return citation_details
+    # back to list of lists
+    processed_paper_ids = [list(log) for log in processed_paper_ids]
+
+    yield pd.DataFrame(), processed_paper_ids
+
+
+def process_citations(
+    citations: Union[Dict[str, Union[str, Sequence[str]]], Sequence],
+    parent: Union[str, Tuple[str, str]],
+) -> pd.DataFrame:
+    """
+    Process the citations data and convert it into a pandas DataFrame.
+
+    Args:
+        citations (Sequence[Dict[str, Union[str, Sequence[str]]]]): A sequence of
+            dictionaries representing the citations data.
+            Each dictionary should have the following keys:
+            - 'intents': A list of strings representing the intents.
+            - 'contexts': A list of strings representing the contexts.
+        parent (str): Parent paper that receives the citation.
+
+    Returns:
+        pd.DataFrame: A pandas DataFrame containing the processed citations data.
+            The DataFrame has the following columns:
+            - 'parent': The parent identifier.
+            - 'child_oa': The child identifier.
+            - 'child_doi': The DOI of the child paper.
+            - 'child_mag': The MAG ID of the child paper.
+            - 'child_pmid': The PMID of the child paper.+
+            - 'influential': Whether the citation is influential or not.
+            - 'intent': The intent of the citation.
+            - 'context': The context of the citation.
+    """
+    rows = []
+    if isinstance(citations, dict):
+        for child_oa, details in citations.items():
+            nested_details = details.get("item", {})
+            context_intents = nested_details.get("contextsWithIntent", [])
+            influential = nested_details.get("isInfluential", "")
+            child_doi = details.get("doi", "")
+            child_mag = details.get("mag", "")
+            child_pmid = details.get("pmid", "")
+            for item in context_intents:
+                context = item.get("context", "")
+                intents = item.get("intents") or [""]
+                for intent in intents:
+                    rows.append(
+                        [
+                            parent,
+                            "",
+                            child_oa,
+                            child_doi,
+                            child_mag,
+                            child_pmid,
+                            "",
+                            influential,
+                            intent,
+                            context,
+                        ]
+                    )
+
+    elif isinstance(citations, list):
+        for citation in citations:
+            parent_doi = parent[0]
+            parent_pmid = parent[1]
+            context_intents = citation.get("contextsWithIntent", [])
+            influential = citation.get("isInfluential", "")
+            ids = citation.get("citingPaper", {}).get("externalIds", {})
+            if not ids:
+                # skip if no ids
+                continue
+            child_doi = ids.get("DOI", "")
+            child_mag = ids.get("MAG", "")
+            child_pmid = ids.get("PubMed", "")
+            child_cid = ids.get("CorpusId", "")
+            for item in context_intents:
+                context = item.get("context", "")
+                intents = item.get("intents") or [""]
+                for intent in intents:
+                    rows.append(
+                        [
+                            parent_doi,
+                            parent_pmid,
+                            "",
+                            child_doi,
+                            child_mag,
+                            child_pmid,
+                            child_cid,
+                            influential,
+                            intent,
+                            context,
+                        ]
+                    )
+
+    # return dataframe
+    return pd.DataFrame(
+        rows,
+        columns=[
+            "parent_doi",
+            "parent_pmid",
+            "child_oa",
+            "child_doi",
+            "child_mag",
+            "child_pmid",
+            "child_cid",
+            "influential",
+            "intent",
+            "context",
+        ],
+    )
--- src/alphafold_impact/pipelines/data_collection_s2/pipeline.py
+++ src/alphafold_impact/pipelines/data_collection_s2/pipeline.py
@@ -4,20 +4,21 @@ generated using Kedro 0.19.1
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
+from alphafold_impact import settings
 from .nodes import (
     load_alphafold_citation_ids,
-    get_alphafold_citation_details
+    get_alphafold_citation_details,
+    fetch_citation_strength,
 )
 
+
 def create_pipeline(**kwargs) -> Pipeline:  # pylint: disable=C0116,W0613
-    return pipeline(
+
+    references_to_alphafold = pipeline(
         [
             node(
                 func=load_alphafold_citation_ids,
-                inputs={
-                    "input_loaders": "cites.input",
-                    "work_id": "params:af_paper"
-                },
+                inputs={"input_loaders": "cites.input", "work_id": "params:af_paper"},
                 outputs="af_citations",
             ),
             node(
@@ -28,10 +29,50 @@ def create_pipeline(**kwargs) -> Pipeline:  # pylint: disable=C0116,W0613
                     "base_url": "params:api.base_url",
                     "direction": "params:api.cited_by",
                     "fields": "params:api.fields",
-                    "perpage": "params:api.perpage"
+                    "perpage": "params:api.perpage",
                 },
-                outputs="intent.alphafold.raw",
-            )
+                outputs=["level.0", "level.0.log.ids.out"],
+            ),
         ],
-        namespace="s2.data_collection"
+        tags=["s2.level.0"],
+        namespace="s2.data_collection.strength",
     )
+
+    citation_strength = pipeline(
+        [
+            node(
+                func=fetch_citation_strength,
+                inputs={
+                    "parent_data": "strength.parent.level",
+                    "logs": "log.ids.in",
+                    "base_url": "params:api.base_url",
+                    "direction": "params:api.cites",
+                    "fields": "params:api.fields",
+                    "perpage": "params:api.perpage",
+                },
+                outputs=["raw", "log.ids.out"],
+            ),
+        ],
+    )
+
+    level_pipelines = [
+        pipeline(
+            citation_strength,
+            inputs={
+                "strength.parent.level": f"s2.data_collection.strength.level.{level}",
+                "log.ids.in": f"s2.data_collection.strength.level.{level}.log.ids.in",
+            },
+            parameters={
+                "api.base_url": "params:s2.data_collection.strength.api.base_url",
+                "api.cites": "params:s2.data_collection.strength.api.cites",
+                "api.fields": "params:s2.data_collection.strength.api.fields",
+                "api.perpage": "params:s2.data_collection.strength.api.perpage",
+            },
+            # outputs="raw",
+            namespace=f"s2.data_collection.strength.level.{level+1}",
+            tags=["s2.levels"],
+        )
+        for level in settings.DYNAMIC_PIPELINES_MAPPING["s2"]
+    ]
+
+    return references_to_alphafold + sum(level_pipelines)
