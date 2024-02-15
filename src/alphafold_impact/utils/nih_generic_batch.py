9a392b097945bdc58d9ba5938ddb67fe6a7432dc
Jack Rasala <jack@jrds.io>
2024-02-15 15:08:21 +0000

    Update function to use text file

--- src/alphafold_impact/utils/nih_generic_batch.py
+++ src/alphafold_impact/utils/nih_generic_batch.py
@@ -6,6 +6,7 @@ See here for installation steps: https://github.com/lhncbc/skr_web_python_api
 
 Get UMLS API key from here: https://uts.nlm.nih.gov/uts/profile
 """
+
 from skr_web_api import Submission
 
 
@@ -13,7 +14,6 @@ def skr_web_python_api_generic_batch(
     email: str,
     api_key: str,
     txt_file_name: str,
-    text: str,
     cmd: str,
     cmdargs: str,
 ) -> str:
@@ -22,9 +22,8 @@ def skr_web_python_api_generic_batch(
     Args:
         email (str): User's email address.
         api_key (str): UTS API key.
-        txt_file_name (str): Name of the text submitted in the job.
-        text (str): Text that is submitted to be tagged. Each record should be on a new line
-            and be in the format {id}|{text}.
+        txt_file_name (str): Name of the text file to submit to be tagged.
+            Each record should be on a new line and be in the format {id}|{text}.
             See example here: https://lhncbc.nlm.nih.gov/ii/about/help/single_line_ID_sample.txt
         cmd (str): Command to be executed.
         cmdargs (str): Arguments for the command. A full list of args can be found:
@@ -37,23 +36,7 @@ def skr_web_python_api_generic_batch(
     inst = Submission(email, api_key)
     inst.set_serviceurl(serviceurl)
     inst.init_generic_batch(cmd, cmdargs)
-    inst.set_batch_file(txt_file_name, text)
+    inst.set_batch_file(txt_file_name)
     inst.form["SingLinePMID"] = True
     response = inst.submit()
     return response.content.decode().replace("NOT DONE LOOP\n", "")
-
-
-txt = """97479605|Higher neonatal cerebral blood flow correlates with worse childhood neurologic outcome.
-98018928|Bupivacaine inhibition of L-type calcium current in ventricular cardiomyocytes of hamster.
-"""
-
-x = skr_web_python_api_generic_batch(
-    email="xxx",
-    api_key="xxx",
-    txt_file_name="test",
-    cmd="MTI",
-    cmdargs="-MoD_PP",
-    text=txt,
-)
-
-print(x)
