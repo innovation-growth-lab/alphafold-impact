f12cdde94a8a8aad79043e86b3ae9414a058fc41
Jack Rasala <jack@jrds.io>
2024-02-17 22:34:13 +0000

    Add full pipeline

--- src/alphafold_impact/pipelines/data_processing_nih_funding_opportunities/pipeline.py
+++ src/alphafold_impact/pipelines/data_processing_nih_funding_opportunities/pipeline.py
@@ -7,9 +7,12 @@ To run this pipeline, use the following command:
 """
 
 from kedro.pipeline import Pipeline, pipeline, node
+from alphafold_impact.utils.nih_mesh_tagging import (
+    generate_text_for_mesh_tagging,
+    skr_web_python_api_generic_batch,
+    mesh_results_to_df,
+)
 from .nodes import clean_nih_funding_opportunites
-
-# from ...utils.generic_batch_file import
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -22,6 +25,34 @@ def create_pipeline(**kwargs) -> Pipeline:
                     "params:nih.data_processing.funding_opportunities.year",
                 ],
                 outputs="nih.data_processing.funding_opportunities.intermediate",
+            ),
+            node(
+                func=generate_text_for_mesh_tagging,
+                inputs=[
+                    "nih.data_processing.funding_opportunities.intermediate",
+                    "params:nih.data_processing.funding_opportunities.mesh_tagging.id_col",
+                    "params:nih.data_processing.funding_opportunities.mesh_tagging.text_col",
+                ],
+                outputs="nih.data_processing.funding_opportunities_mesh_tagging_input_text.intermediate",
+            ),
+            node(
+                func=skr_web_python_api_generic_batch,
+                inputs=[
+                    "params:mesh_tagging.email",
+                    "params:mesh_tagging.umls_api_key",
+                    "params:nih.data_processing.funding_opportunities.mesh_tagging.batch_name",
+                    "nih.data_processing.funding_opportunities_mesh_tagging_input_text.intermediate",
+                    "params:nih.data_processing.funding_opportunities.mesh_tagging.cmd",
+                    "params:nih.data_processing.funding_opportunities.mesh_tagging.cmdargs",
+                ],
+                outputs="nih.data_processing.funding_opportunities_mesh_tagging_output_text.intermediate",
+            ),
+            node(
+                func=mesh_results_to_df,
+                inputs=[
+                    "nih.data_processing.funding_opportunities_mesh_tagging_output_text.intermediate",
+                ],
+                outputs="nih.data_processing.funding_opportunities_mesh_tagging_output_df.intermediate",
             ),
         ],
     )
