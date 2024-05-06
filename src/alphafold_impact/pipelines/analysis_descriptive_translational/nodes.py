84441589829f1fe2a7b1d28b6bc12fee7085ab26
ampudia19 <dampudiavicente@gmail.com>
2024-05-06 18:02:38 +0200

    cc in analysis

--- src/alphafold_impact/pipelines/analysis_descriptive_translational/nodes.py
+++ src/alphafold_impact/pipelines/analysis_descriptive_translational/nodes.py
@@ -25,3 +25,42 @@ def merge_inputs(**kwargs):
     return pd.concat(
         [kwargs["alphafold_data"], kwargs["ct_data"], kwargs["other_data"]]
     )
+
+def get_cc_counts(data: pd.DataFrame, icite_data: pd.DataFrame):
+    # change level to -1 for parent_id "W3177828909", "W3211795435", "W3202105508"
+    data.loc[data["id"].isin(["W3177828909", "W3211795435", "W3202105508"]), "level"] = -1
+
+    # preprocess icite data
+    icite_data["pmid"] = icite_data["pmid"].astype(str)
+
+    logger.info("Merging chains with iCite data")
+
+    # Merge on 'pmid'
+    data_pmid = data.merge(
+        icite_data[['pmid', 'cited_by_clin']],
+        how='left',
+        on='pmid'
+    )
+
+    # Remove duplicates
+    data_pmid = data_pmid.drop_duplicates(subset=["parent_id", "id", "level", "source"])
+
+    # Merge on 'doi'
+    data_doi = data.merge(
+        icite_data[['doi', 'cited_by_clin']],
+        how='left',
+        on='doi'
+    )
+
+    # Remove duplicates
+    data_doi = data_doi.drop_duplicates(subset=["parent_id", "id", "level", "source"])
+
+    # Combine the results
+    combined_data = pd.concat([data_pmid, data_doi]).drop_duplicates()
+
+    # how many CC
+        # strong and weak
+    # how many strong articles
+    # patenst
+
+    return combined_data
--- src/alphafold_impact/pipelines/analysis_descriptive_translational/pipeline.py
+++ src/alphafold_impact/pipelines/analysis_descriptive_translational/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.19.1
 """
 
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import load_input_data, merge_inputs
+from .nodes import load_input_data, merge_inputs, get_cc_counts
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -13,8 +13,8 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 load_input_data,
                 inputs={
-                    "data": "oa.data_processing.depth.all.primary",
-                    "source": "params:analysis.source.af"
+                    "data": "chains.complete_links.id.primary",
+                    "source": "params:analysis.source.af",
                 },
                 outputs="af.level0_data",
             ),
@@ -22,7 +22,7 @@ def create_pipeline(**kwargs) -> Pipeline:
                 load_input_data,
                 inputs={
                     "data": "oa.data_processing.structural_biology.depth.reassigned.ct.intermediate",
-                    "source": "params:analysis.source.ct"
+                    "source": "params:analysis.source.ct",
                 },
                 outputs="ct.level0_data",
             ),
@@ -30,20 +30,28 @@ def create_pipeline(**kwargs) -> Pipeline:
                 load_input_data,
                 inputs={
                     "data": "oa.data_processing.structural_biology.depth.reassigned.other.intermediate",
-                    "source": "params:analysis.source.other"
+                    "source": "params:analysis.source.other",
                 },
                 outputs="other.level0_data",
             ),
             node(
                 merge_inputs,
-                inputs=
-                {
+                inputs={
                     "alphafold_data": "af.level0_data",
                     "ct_data": "ct.level0_data",
                     "other_data": "other.level0_data",
                 },
                 outputs="analysis.descriptive.level0_data",
-            )
+            ),
+            node(
+                get_cc_counts,
+                inputs={
+                    "data": "analysis.descriptive.level0_data",
+                    "icite_data": "pubmed.data_processing.icite.intermediate",
+                },
+                outputs="analysis.descriptive.level0_data_with_cc_counts",
+                tags=["cc_counts"]
+            ),
         ],
         tags=["analysis_descriptive_translational"],
     )
