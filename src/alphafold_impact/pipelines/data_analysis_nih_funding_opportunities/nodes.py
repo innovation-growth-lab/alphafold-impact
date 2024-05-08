2ffc61f37af797cecec92eadb48fed644129f30e
Jack Rasala <jack@jrds.io>
2024-05-08 15:23:21 +0100

    Update typehints and docstrings

--- src/alphafold_impact/pipelines/data_analysis_nih_funding_opportunities/nodes.py
+++ src/alphafold_impact/pipelines/data_analysis_nih_funding_opportunities/nodes.py
@@ -4,6 +4,7 @@ related MeSH data.
 """
 
 import pandas as pd
+from typing import List
 
 
 def _find_lower_lvl_mesh_terms(
@@ -15,10 +16,11 @@ def _find_lower_lvl_mesh_terms(
 
     Args:
         full_mesh_tree: A pandas DataFrame with a 'tree_number' column.
-        tree_number: A string to match the start of the 'tree_number' entries.
+        tree_numbers: A string to match the start of the 'tree_number' entries.
 
     Returns:
-        A DataFrame containing the rows where 'tree_number' starts with the given string.
+        pd.DataFrame: A DataFrame containing the rows where 'tree_number' starts
+            with the given string.
     """
     return full_mesh_tree[
         full_mesh_tree["tree_number"].apply(
@@ -29,15 +31,20 @@ def _find_lower_lvl_mesh_terms(
     ]
 
 
-def _list_mesh_duis(subfield_mesh_dict: dict, full_mesh_tree: pd.DataFrame) -> list:
+def _list_mesh_duis(
+    subfield_mesh_dict: dict, full_mesh_tree: pd.DataFrame
+) -> List[str]:
     """
     Return list of MeSH DUIs
 
     Args:
         subfield_mesh_dict (dict): Dictionary with keys of MeSH names and
             list of MeSH tree numbers for the values
-        full_mesh_tree (dict): Full collection of MeSH names,
+        full_mesh_tree (pd.DataFrame): Full collection of MeSH names,
             DUIs and tree numbers
+
+    Returns:
+        list: A list of MeSH DUIs
     """
     return [
         DUI
@@ -57,7 +64,7 @@ def _search_text_in_multiple_cols(
     Args:
         df (pd.DataFrame): The DataFrame to search in.
         columns (list[str]): The names of the columns to perform the search.
-        texts (list[str]): The texts to search for.
+        search_terms (list[str]): The terms to search for.
 
     Returns:
         pd.DataFrame: A DataFrame containing the rows where any text is found
@@ -85,6 +92,33 @@ def find_relevant_funding_opps(
     search_terms: list,
     search_cols: list,
 ) -> pd.DataFrame:
+    """
+    Find relevant funding opportunities for a specific subfield by using both
+    MeSH terms and keyword search. This function integrates results from two
+    sources: MeSH term matching and keyword search.
+
+    Args:
+        nih_funding_opps (pd.DataFrame): DataFrame containing NIH funding
+            opportunities data.
+        nih_funding_opps_mesh_tags (pd.DataFrame): DataFrame containing NIH
+            funding opportunities linked to MeSH terms.
+        full_mesh_tree (pd.DataFrame): DataFrame containing the full MeSH tree
+            structure, used to identify lower level MeSH terms.
+        subfield_label (str): Label of the subfield for which to find funding
+            opportunities (e.g., "Structural Biology").
+        subfield_mesh_dict (dict): Dictionary where keys are MeSH names
+            associated with the subfield and values are lists of MeSH tree numbers.
+        search_terms (list): List of keywords to search for within
+            the specified columns of `nih_funding_opps`.
+        search_cols (list): List of column names in `nih_funding_opps` where
+            the `search_terms` will be searched.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing all relevant funding opportunities
+        found either through MeSH term matching or keyword searching,
+        deduplicated and annotated with the source of the match ('nih mesh' or
+        'nih search') and the subfield label.
+    """
     # Find relevant funding opps based on MeSH term
     mesh_duis = _list_mesh_duis(subfield_mesh_dict, full_mesh_tree)
     nih_funding_opps_mesh_tags["mesh_term"] = nih_funding_opps_mesh_tags[
