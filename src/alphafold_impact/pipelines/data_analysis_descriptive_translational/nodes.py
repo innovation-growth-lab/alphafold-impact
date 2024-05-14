454e6e6cad83f19bea87c5c8b39f025d974b6667
ampudia19 <dampudiavicente@gmail.com>
2024-05-14 11:04:40 +0200

    minor changes translational

--- src/alphafold_impact/pipelines/data_analysis_descriptive_translational/nodes.py
+++ src/alphafold_impact/pipelines/data_analysis_descriptive_translational/nodes.py
@@ -6,12 +6,46 @@ generated using Kedro 0.19.1
 import logging
 import pandas as pd
 from Bio import Entrez
-from ..data_processing_chains.nodes import (  # pylint: disable=E0402
-    get_entrez_ptype_pmid,
-)
+
 
 Entrez.email = "david.ampudia@nesta.org.uk"
 logger = logging.getLogger(__name__)
+
+
+def get_entrez_ptype_pmid(pmid: str) -> str:
+    """
+    Retrieves the publication type for a given PubMed ID (PMID).
+
+    Args:
+        pmid (str): The PubMed ID (PMID) of the publication.
+
+    Returns:
+        str: The publication type of the specified publication.
+    """
+    try:
+        stream = Entrez.efetch(db="pubmed", id=pmid, retmax="1")
+        record = Entrez.read(stream)
+        pub_type = str(
+            record["PubmedArticle"][0]["MedlineCitation"]["Article"].get(
+                "PublicationTypeList"
+            )[0]
+        )
+
+        year = record["PubmedArticle"][0]["MedlineCitation"]["Article"]["ArticleDate"][
+            0
+        ].get("Year")
+        month = record["PubmedArticle"][0]["MedlineCitation"]["Article"]["ArticleDate"][
+            0
+        ].get("Month")
+        day = record["PubmedArticle"][0]["MedlineCitation"]["Article"]["ArticleDate"][
+            0
+        ].get("Day")
+
+        pub_date = f"{year}-{month}-{day}"
+        return pd.Series([pub_type, pub_date])
+
+    except Exception:  # pylint: disable=broad-except
+        return pd.Series([None, None])
 
 
 def load_input_data(
@@ -32,7 +66,7 @@ def load_input_data(
     data["level"] = data["level"].astype(str)
 
     logger.info("Filter for level 0")
-    data = data[data["level"] == "0"]
+    data = data[(data["level"] == "0") | (data["level"] == "-1")]
 
     logger.info("Create columns with source")
     data["source"] = source
@@ -60,26 +94,38 @@ def merge_inputs(**kwargs):
 
 def preprocess_sb_data(data: pd.DataFrame):
     """Preprocess the data."""
+
+    # create a row that is af's
+    new_row = {
+        "id": "W3177828909",
+        "doi": "10.1038/s41586-021-03819-2",
+        "pmid": "34265844",
+        "title": "Highly accurate protein structure prediction with AlphaFold",
+        "publication_date": "2021-07-15",
+        "level": "-1",
+        "parent_level": "-2",
+        "source": "af",
+    }
+
+    data.loc[len(data)] = new_row
+
+    # change level to -1 for parent_id "W3177828909", "W3211795435", "W3202105508"
+    data.loc[data["id"].isin(["W3211795435", "W3202105508"]), "level"] = "-1"
+    data.loc[data["id"].isin(["W3211795435", "W3202105508"]), "parent_level"] = "-2"
+
     # create a dictionary mapping id to publication_date
     id_date_dict = data.set_index("id")["publication_date"].to_dict()
 
     # create a new column 'parent_publication_date' by mapping the 'parent_id' column to the dictionary
     data["parent_publication_date"] = data["parent_id"].map(id_date_dict)
 
-    # if parent_id is W3177828909, set parent_publication_date to 2021-06-25
-    data.loc[data["parent_id"] == "W3177828909", "parent_publication_date"] = (
-        "2021-06-25"
-    )
-
-    # change level to -1 for parent_id "W3177828909", "W3211795435", "W3202105508"
-    data.loc[
-        data["id"].isin(["W3177828909", "W3211795435", "W3202105508"]), "level"
-    ] = -1
-
+    # force level and parent_level to be str
+    data["level"] = data["level"].astype(str)
+    data["parent_level"] = data["parent_level"].astype(str)
     return data
 
 
-def get_cc_counts(data: pd.DataFrame, icite_data: pd.DataFrame):
+def get_cc_papers(data: pd.DataFrame, icite_data: pd.DataFrame):
     """
     Get citation counts for the given data.
 
@@ -132,13 +178,85 @@ def get_cc_counts(data: pd.DataFrame, icite_data: pd.DataFrame):
     # drop dup
     combined_data.drop_duplicates(subset=["id", "cited_by_clin"], inplace=True)
 
-    combined_data["publication_type"] = combined_data["cited_by_clin"].apply(
-        get_entrez_ptype_pmid
-    )
+    combined_data[["ca_publication_type", "ca_publication_date"]] = combined_data[
+        "cited_by_clin"
+    ].apply(get_entrez_ptype_pmid)
 
     return combined_data
 
 
-# strong and weak
-# how many strong articles
-# patenst
+def get_cc_moderators(
+    cc_data: pd.DataFrame,
+    sc_data_af: pd.DataFrame,
+    sc_data_ct: pd.DataFrame,
+    pdb_data: pd.DataFrame,
+):
+    # Combine the 'id' columns of the three DataFrames into one Series
+    combined_ids = pd.concat([sc_data_af["id"], sc_data_ct["id"]])
+
+    # Check if the 'id' values in cc_data are in combined_ids
+    cc_data["id_in_sc_data"] = cc_data["id"].isin(combined_ids)
+
+    # Check if the 'id' values in cc_data are in pdb_data
+    cc_data["id_in_pdb_data"] = cc_data["id"].isin(pdb_data["id"])
+
+    # add R_free and resolution metrics
+    cc_data = pd.merge(
+        cc_data,
+        pdb_data[["id", "R_free", "resolution"]],
+        on="id",
+        how="left",
+    )
+
+
+    # check if cited_by_clin is in pdb_data
+    cc_data["cited_by_clin_in_pdb_data"] = cc_data["cited_by_clin"].isin(
+        pdb_data["pmid"]
+    )
+
+    return cc_data
+
+
+def get_patent_papers(data: pd.DataFrame, patent_data: pd.DataFrame):
+    """
+    Get patent counts for the given data.
+
+    Args:
+        data (pd.DataFrame): The input data containing information about articles.
+        patent_data (pd.DataFrame): The input data containing patent information.
+
+    Returns:
+        pd.DataFrame: The combined data with patent counts.
+
+    """
+    # merge patent_data on data
+    matched_data = data.merge(
+        patent_data, how="inner", left_on="doi", right_on="NPL Resolved External ID(s)"
+    )
+    return matched_data
+
+
+def get_patent_moderators(
+    pc_data: pd.DataFrame,
+    sc_data_af: pd.DataFrame,
+    sc_data_ct: pd.DataFrame,
+    pdb_data: pd.DataFrame,
+):
+    # Combine the 'id' columns of the three DataFrames into one Series
+    combined_ids = pd.concat([sc_data_af["id"], sc_data_ct["id"]])
+
+    # Check if the 'id' values in pc_data are in combined_ids
+    pc_data["id_in_sc_data"] = pc_data["id"].isin(combined_ids)
+
+    # add R_free and resolution metrics
+    pc_data = pd.merge(
+        pc_data,
+        pdb_data[["id", "R_free", "resolution"]],
+        on="id",
+        how="left",
+    )
+
+    # Check if the 'id' values in pc_data are in pdb_data
+    pc_data["id_in_pdb_data"] = pc_data["id"].isin(pdb_data["id"])
+
+    return pc_data
--- src/alphafold_impact/pipelines/data_analysis_descriptive_translational/pipeline.py
+++ src/alphafold_impact/pipelines/data_analysis_descriptive_translational/pipeline.py
@@ -4,7 +4,15 @@ generated using Kedro 0.19.1
 """
 
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import load_input_data, merge_inputs, preprocess_sb_data, get_cc_counts
+from .nodes import (
+    load_input_data,
+    merge_inputs,
+    preprocess_sb_data,
+    get_cc_papers,
+    get_cc_moderators,
+    get_patent_papers,
+    get_patent_moderators,
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -13,20 +21,11 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 load_input_data,
                 inputs={
-                    "data": "oa.data_processing.depth.other.primary",
-                    "source": "params:analysis.source.other",
-                },
-                outputs="analysis.other.level0_data",
-                tags=["other_descriptive"]
-            ),
-            node(
-                load_input_data,
-                inputs={
-                    "data": "oa.data_processing.depth.primary",
+                    "data": "oa.data_processing.depth.all.primary",
                     "source": "params:analysis.source.af",
                 },
                 outputs="analysis.af.level0_data",
-                tags=["af_descriptive"]
+                tags=["af_descriptive"],
             ),
             node(
                 load_input_data,
@@ -35,7 +34,16 @@ def create_pipeline(**kwargs) -> Pipeline:
                     "source": "params:analysis.source.ct",
                 },
                 outputs="analysis.ct.level0_data",
-                tags=["ct_descriptive"]
+                tags=["ct_descriptive"],
+            ),
+            node(
+                load_input_data,
+                inputs={
+                    "data": "oa.data_processing.depth.other.primary",
+                    "source": "params:analysis.source.other",
+                },
+                outputs="analysis.other.level0_data",
+                tags=["other_descriptive"],
             ),
             node(
                 merge_inputs,
@@ -45,21 +53,53 @@ def create_pipeline(**kwargs) -> Pipeline:
                     "other_data": "analysis.other.level0_data",
                 },
                 outputs="analysis.descriptive.level0_data",
-                tags="descriptive_merge"
+                tags="descriptive_merge",
             ),
             node(
                 preprocess_sb_data,
                 inputs="analysis.descriptive.level0_data",
                 outputs="analysis.descriptive.level0_data.processed",
+                tags="descriptive_preprocess",
             ),
             node(
-                get_cc_counts,
+                get_cc_papers,
                 inputs={
-                    "data": "analysis.descriptive.level0_data",
+                    "data": "analysis.descriptive.level0_data.processed",
                     "icite_data": "pubmed.data_processing.icite.intermediate",
                 },
-                outputs="analysis.descriptive.level0_data_with_cc_counts",
-                tags=["cc_counts"]
+                outputs="analysis.descriptive.level0_data_with_cc",
+                tags=["cc_counts"],
+            ),
+            node(
+                get_cc_moderators,
+                inputs={
+                    "cc_data": "analysis.descriptive.level0_data_with_cc",
+                    "sc_data_af": "chains.complete_strong_links.id.primary",
+                    "sc_data_ct": "chains.complete_strong_links.id.ct.primary",
+                    "pdb_data": "pdb.entries.intermediate",
+                },
+                outputs="analysis.descriptive.level0_data_with_moderators_cc",
+                tags=["cc_moderators", "moderators"],
+            ),
+            node(
+                get_patent_papers,
+                inputs={
+                    "data": "analysis.descriptive.level0_data.processed",
+                    "patent_data": "lens.data_processing.primary",
+                },
+                outputs="analysis.descriptive.level0_data_with_patents",
+                tags=["patent_counts"],
+            ),
+            node(
+                get_patent_moderators,
+                inputs={
+                    "pc_data": "analysis.descriptive.level0_data_with_patents",
+                    "sc_data_af": "chains.complete_strong_links.id.primary",
+                    "sc_data_ct": "chains.complete_strong_links.id.ct.primary",
+                    "pdb_data": "pdb.entries.intermediate",
+                },
+                outputs="analysis.descriptive.level0_data_with_moderators_pc",
+                tags=["pc_moderators", "moderators"],
             ),
         ],
         tags=["analysis_descriptive_translational"],
